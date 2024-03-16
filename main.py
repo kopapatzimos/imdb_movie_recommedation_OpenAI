f5a72fa1ba70e01382b5b0b763e188547c81864a
kopapatzimos <72562258+kopapatzimos@users.noreply.github.com>
2024-03-16 14:31:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,33 +72,3 @@ while True:
     response = process_query(user_input)
     print("Bot:", response)
 
-
-# from langchain.prompts import PromptTemplate
-
-# template = """You are a movie recommender system that help users to find anime that match their preferences. 
-# Use the following pieces of context to answer the question at the end. 
-# For each question, suggest three anime, with a short description of the plot and the reason why the user migth like it.
-# If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-# {context}
-
-# Question: {question}
-# Your response:"""
-
-
-# PROMPT = PromptTemplate(
-#     template=template, input_variables=["context", "question"])
-
-# chain_type_kwargs = {"prompt": PROMPT}
-
-# llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0) 
-
-# qa = RetrievalQA.from_chain_type(llm=llm, 
-#     chain_type="stuff", 
-#     retriever=docsearch.as_retriever(),
-#     return_source_documents=True, 
-#     chain_type_kwargs=chain_type_kwargs)
-
-# query = "I'm looking for an action anime with animals, any suggestions?"
-# result = qa({'query':query})
-# print(result['result'])
